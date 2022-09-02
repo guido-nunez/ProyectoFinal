de6549e676e3ffd4a35a8475c7570c42172feab6
guido <guido-nunez@hotmail.com>
2022-09-01 22:17:29 -0300

    Edicion de Experiencia Laboral
    Se implemento la funcionalidad de agregar, editar y borrar en el apartado de experiencia laboral

---
+++ ProyectoFinal/BackEnd/src/main/java/com/portfolio/web/Dto/dtoExperiencia.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.web.Dto;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ *
+ * @author guido
+ */
+public class dtoExperiencia {
+    @NotBlank
+    private String nombreE;
+    @NotBlank
+    private String descripcionE;
+    
+    //Constructor
+
+    public dtoExperiencia() {
+    }
+
+    public dtoExperiencia(String nombreE, String descripcionE) {
+        this.nombreE = nombreE;
+        this.descripcionE = descripcionE;
+    }
+    
+    //Getter and Setters
+
+    public String getNombreE() {
+        return nombreE;
+    }
+
+    public void setNombreE(String nombreE) {
+        this.nombreE = nombreE;
+    }
+
+    public String getDescripcionE() {
+        return descripcionE;
+    }
+
+    public void setDescripcionE(String descripcionE) {
+        this.descripcionE = descripcionE;
+    }
+    
+}
