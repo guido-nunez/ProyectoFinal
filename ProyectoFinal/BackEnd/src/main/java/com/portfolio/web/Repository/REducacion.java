471c3e4582fc65166b3b5ea0f45e09948a85406b
guido <guido-nunez@hotmail.com>
2022-09-04 19:57:18 -0300

    agregar eliminar y editar - educación
    implementación de botones de eliminación, edición y adicionar elementos en apartado de educación

---
+++ ProyectoFinal/BackEnd/src/main/java/com/portfolio/web/Repository/REducacion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.portfolio.web.Repository;
+
+import com.portfolio.web.Entity.Educacion;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author guido
+ */
+@Repository
+public interface REducacion extends JpaRepository<Educacion, Integer>{
+    public Optional<Educacion> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
